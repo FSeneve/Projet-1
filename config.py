c0a4daf00e2fe083081e1d8f2bf723654ea5fc02
Francis Seneve <francisseneve@gmail.com>
2022-08-05 11:05:54 +0100

    Project 1 Fyyur

--- config.py
+++ config.py
@@ -10,4 +10,7 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Francis@localhost:5432/fyyurproject'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+WTF_CSRF_ENABLED = False

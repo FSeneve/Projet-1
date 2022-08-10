ad2705ea352bdee46b8822b602cc76acd3e0df29
Francis Seneve <francisseneve@gmail.com>
2022-08-11 00:18:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import dateutil.parser
 import babel
 from flask import Flask, render_template, request, Response, flash, redirect, url_for
 from models import db, Venue, Artist, Show
+from sqlalchemy.orm import backref
+from sqlalchemy.sql import func
 from flask_moment import Moment
 from flask_migrate import Migrate
 import logging
